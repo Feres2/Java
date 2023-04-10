4465a2cb48a95c0c26c12a8cd432abf36090a97d
Feres2 <feresalyani@gmail.com>
2023-04-10 18:25:25 +0200

    feres

---
+++ Assigments/Practice/First Java Program/JavaProgram.java
@@ -0,0 +1,9 @@
+public class JavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is feres ");
+        System.out.println("I am 20 y.o ");
+        System.out.println("My hometown is benarous ");
+
+
+    }
+}
